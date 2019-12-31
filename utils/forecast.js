030df7e3778b9148471bbe57db097eba11f606bf
didindinn <58913202+didindinn@users.noreply.github.com>
2019-12-31 14:35:51 +0330

    Update forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -14,9 +14,9 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       return callback(
         undefined,
-        `${data.daily.data[0].summary} دمای کنونی هوا ${data.currently.temperature} درجه سانتی گراد میباشد."\n"
-          بیشترین دما: ${data.daily.data[0].temperatureHigh}درجه سانتی گراد میباشد."\n"
-          کمترین دما: ${data.daily.data[0].temperatureLow}درجه سانتی گراد میباشد."\n"
+        `${data.daily.data[0].summary} دمای کنونی هوا ${data.currently.temperature} درجه سانتی گراد میباشد. \n 
+          بیشترین دما: ${data.daily.data[0].temperatureHigh}درجه سانتی گراد میباشد. \n 
+          کمترین دما: ${data.daily.data[0].temperatureLow}درجه سانتی گراد میباشد. \n 
           شانس بارندگی امروز شما ${data.currently.precipProbability}% می باشد.`
       );
     }
